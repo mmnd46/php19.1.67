02b986ae4ebe87bd49ae8b45efcce20b889033fe
mmnd46 <154506757+mmnd46@users.noreply.github.com>
2024-01-19 14:24:13 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
+    <title>My Home Page</title>
+
+    <!-- #### ส่วนของ CSS ที่เตรียมมาให้เพื่อใช้ตกแต่งเว็บไซต์ ### -->
+    <link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
+    <link href="css/style2.css" rel="stylesheet">
+    <!-- ############################### -->
+</head>
+
+<!-- ใส่รูปภาพพื้นหลัง -->
+<body background = bg1.JPG>
+    <div class="row">
+        <div class="column">
+            <div class="column-bottom">
+
+                <!-- ใส่ชื่อเว็บที่นี่ -->
+
+            </div>
+        </div>
+			
+		<div class="column">
+            <!-- ใส่ลิงก์ในหน้าเว็บที่นี่ -->
+            <div class="column-right">
+                <p>
+                <a href="home.php">Home</a> <br>
+                <a href="https://worldheritagesite.onep.go.th/sitedetail/3">Location 1</a> <br>
+                <a href="https://worldheritagesite.onep.go.th/sitedetail/2">Location 2</a> <br>
+                <a href="https://th.wikipedia.org/wiki/%E0%B9%81%E0%B8%AB%E0%B8%A5%E0%B9%88%E0%B8%87%E0%B9%82%E0%B8%9A%E0%B8%A3%E0%B8%B2%E0%B8%93%E0%B8%84%E0%B8%94%E0%B8%B5%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B9%80%E0%B8%8A%E0%B8%B5%E0%B8%A2%E0%B8%87">Location 3</a> <br>
+                <a href="https://www.finearts.go.th/main/view/8207-%E0%B8%AD%E0%B8%B8%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C%E0%B8%A8%E0%B8%A3%E0%B8%B5%E0%B9%80%E0%B8%97%E0%B8%9E">Location 4</a> <br>
+                <a href="https://th.wikipedia.org/wiki/%E0%B8%AD%E0%B8%B8%E0%B8%97%E0%B8%A2%E0%B8%B2%E0%B8%99%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%A7%E0%B8%B1%E0%B8%95%E0%B8%B4%E0%B8%A8%E0%B8%B2%E0%B8%AA%E0%B8%95%E0%B8%A3%E0%B9%8C%E0%B8%9E%E0%B8%A3%E0%B8%B0%E0%B8%99%E0%B8%84%E0%B8%A3%E0%B8%A8%E0%B8%A3%E0%B8%B5%E0%B8%AD%E0%B8%A2%E0%B8%B8%E0%B8%98%E0%B8%A2%E0%B8%B2">Location 5</a> <br>
+                <a href="gallery.php">Gallery</a> <br>
+                <a href="https://www.facebook.com/">Facebook |</a> 
+                <a href="https://www.instagram.com/">Instagram |</a> 
+                <a href="https://twitter.com/">Twitter</a>
+                
+
+                
+
+                </P>
+            </div>
+		</div>
+		
+	</div>
+        <!-- ################### -->
+</body>
+</html>
